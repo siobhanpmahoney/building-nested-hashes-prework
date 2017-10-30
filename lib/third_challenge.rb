91570aea6f7b4fd13263fae861e10e54e6f3b6fb
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-30 16:49:51 +0000

    automatically backed up by learn

---
+++ lib/third_challenge.rb
@@ -0,0 +1,32 @@
+def third_challenge
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {
+        name: "Lord Montague",
+        age: "53"
+        },
+      :matriarch => {
+        name: "Lady Montague",
+        age: "54"
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+   }
+  }
+
+
+end
